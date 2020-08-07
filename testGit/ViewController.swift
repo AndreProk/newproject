4dec765aba228bb8a7e30ca28f342215bc8b3e9d
Andre <andre@Andres-MacBook-Air.local>
2020-08-07 19:34:02 +0300

    created

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Andre  on 8/7/20.
+//  Copyright © 2020 Dre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
